c215420d3d008888f56618e34df4350275577266
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2021-12-19 20:26:00 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,9 +3,9 @@
 // Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
 // Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
 // В результате, в консоли будут выведены такие сообщения.
-const AllLiEl = document.querySelectorAll('.item');
-console.log(`Number of categories: ${AllLiEl.length}`)
+const LiEl = document.querySelectorAll('.item');
+console.log(`Number of categories: ${LiEl.length}`)
 
-const AllLiElEl = [...AllLiEl].map(categories => `Категория: ${categories.children[0].textContent}
+const AllLiEl = [...LiEl].map(categories => `Категория: ${categories.children[0].textContent}
 Количество элементов: ${categories.children[1].children.length}`).join(".\n");
-console.log(AllLiElEl);
+console.log(AllLiEl);
